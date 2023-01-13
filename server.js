fb3d6327b5efff1599371a07a347dfe6c182f67d
sanjaybrijesh209 <107564577+sanjaybrijesh209@users.noreply.github.com>
2023-01-12 19:26:20 -0500

    assignment1

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: ____sanjay_brijesh_________________ Student ID: ______156653214________ Date: ________________
+*
+*  Online (Cyclic) URL: _______https://teal-chiton-tux.cyclic.app/________________________________________________
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
